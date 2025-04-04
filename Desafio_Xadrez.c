afb7d41a6f3f12f09f24fd1c41f03dcad1a1afab
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-04-04 15:29:12 +0000

    desafio xadrez aventureiro

--- Desafio_Xadrez.c
+++ Desafio_Xadrez.c
@@ -4,6 +4,8 @@ int main(){
     //mover torre cinco casas para direita
 
     int t = 1, b = 1, r = 1;
+    int movimentocompleto = 1; //flag para controlar o movimento em "L"
+    int opcao;
     while (t <= 5)
     {
         printf("Direita\n"); //imprime a direção do movimento
@@ -22,6 +24,31 @@ int main(){
     {
         printf("Esquerda\n"); //imprime a direção do movimento
     }
+    printf("\n");
+
+    //mover cavalo com loops
+    printf("Escolha uma opção de movimento para o Cavalo\n");
+    printf("1. cima, cima, direita\n");
+    printf("2. direita, direita, cima\n");
+    scanf("%d", &opcao);
+
+    if(opcao == 1){
+    while(movimentocompleto--) {
+        for(int i = 0; i < 2; i++) {
+            printf("Cima\n"); //imprime cima duas vezes
+        }
+        printf("Direita\n"); //imprime direita uma vez
+    }
+    } else {
+         //mover cavalo com loops
+    while(movimentocompleto--) {
+        for(int i = 0; i < 2; i++) {
+            printf("Direita\n"); //imprime direita duas vezes
+        }
+        printf("Cima\n"); //imprime cima uma vez
+    }
+    }
+
     
     return 0;
 }
